40c8282f11e86c56be20fd070b4c540f551c6e54
young <zorag@qq.com>
2019-12-03 14:19:02 +0800

    跨域参数改配置文件处

--- service/select-number-server/select-number-build-18001/src/main/java/com/muhuan/select/build/config/CorsConfig.java
+++ service/select-number-server/select-number-build-18001/src/main/java/com/muhuan/select/build/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.muhuan.select.build.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -15,11 +16,18 @@ import org.springframework.web.filter.CorsFilter;
  **/
 @Configuration
 public class CorsConfig {
+    @Value("${cors.addAllowedOrigin}")
+    private String addAllowedOrigin;
+    @Value("${cors.addAllowedHeader}")
+    private String addAllowedHeader;
+    @Value("${cors.addAllowedMethod}")
+    private String addAllowedMethod;
+
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("http://www.keep-alive.site"); // 1 设置访问源地址
-        corsConfiguration.addAllowedHeader("*"); // 2 设置访问源请求头
-        corsConfiguration.addAllowedMethod("*"); // 3 设置访问源请求方法
+        corsConfiguration.addAllowedOrigin(addAllowedOrigin); // 1 设置访问源地址
+        corsConfiguration.addAllowedHeader(addAllowedHeader); // 2 设置访问源请求头
+        corsConfiguration.addAllowedMethod(addAllowedMethod); // 3 设置访问源请求方法
         return corsConfiguration;
     }
 
